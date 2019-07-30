6a18e406023e7699b40af919df9a04b3480f0153
Mickeyha <mickeyha65@gmail.com>
2019-07-30 16:02:08 +0800

    [GitUserList-4] add APIs

--- app/src/main/java/com/example/githubuserinfo/api/GithubService.kt
+++ app/src/main/java/com/example/githubuserinfo/api/GithubService.kt
@@ -1,8 +1,40 @@
 package com.example.githubuserinfo.api
 
-class GithubService {
+import com.example.githubuserinfo.model.UserDetailInfo
+import com.example.githubuserinfo.model.UserInfo
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import retrofit2.http.Url
+
+interface GithubService {
 
     companion object {
         const val GITHUB_BASE_URL = "https://api.github.com"
     }
+
+    /**
+     * @param since get users since #since(id)
+     * @param perPage get #perpage users at one time
+     */
+    @GET("/users")
+    fun getUsers (
+        @Query("since") since: Int,
+        @Query("per_page") perPage: Int
+    ): Single<List<UserInfo>>
+
+    /**
+     * get user data with url
+     */
+    @GET
+    fun getUsers (
+        @Url url: String
+    ): Single<List<UserInfo>>
+
+
+    @GET("/users/{username}")
+    fun getUserDetail (
+        @Path(value = "username") username: String
+    ): Single<UserDetailInfo>
 }
--- app/src/main/java/com/example/githubuserinfo/di/AppModule.kt
+++ app/src/main/java/com/example/githubuserinfo/di/AppModule.kt
@@ -1,5 +1,7 @@
 package com.example.githubuserinfo.di
 
+import com.example.githubuserinfo.api.GithubService
+import com.example.githubuserinfo.api.GithubService.Companion.GITHUB_BASE_URL
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -11,6 +13,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 val networkModule = module {
     single { generateOkHttpClient() }
     single { generateRetrofit(get()) }
+    single { generateGithubService(get()) }
 }
 
 fun generateOkHttpClient(): OkHttpClient =
@@ -20,10 +23,13 @@ fun generateOkHttpClient(): OkHttpClient =
 
 fun generateRetrofit(okHttpClient: OkHttpClient): Retrofit =
     Retrofit.Builder()
-        .baseUrl("")
+        .baseUrl(GITHUB_BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
+
+fun generateGithubService(retrofit: Retrofit): GithubService =
+        retrofit.create(GithubService::class.java)
 
 val appModule = listOf(networkModule)
