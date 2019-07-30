1ec280f21250a20eb542bc6d3fe9c962ab260d87
Mickeyha <mickeyha65@gmail.com>
2019-07-30 17:29:19 +0800

    [GitUserList-18] dataBinding MainActivity

--- app/src/main/java/com/example/githubuserinfo/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubuserinfo/ui/MainActivity.kt
@@ -2,15 +2,27 @@ package com.example.githubuserinfo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil
 import com.example.githubuserinfo.R
+import com.example.githubuserinfo.databinding.ActivityMainBinding
+import com.example.githubuserinfo.viewmodel.UserInfoViewModel
 
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
+    val viewModel by viewModel<UserInfoViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this@MainActivity, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.userInfoViewModel = viewModel
+
+
         setSupportActionBar(toolbar)
     }
 }
